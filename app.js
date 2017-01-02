b14b1bff4ad98af725c1b86375f7c1f354856e98
kishi <kishiinnovation@gmail.com>
2017-01-02 21:00:07 +0900

    Music plays in Chrome too. Make area.

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+var three = require("three");
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 app.use(express.static(__dirname + '/public'));
 
-io.on("connection", function(socket) {
+//var userHash = {};
 
+io.on("connection", function(socket) {
+    socket.on("connected", function(position, rotation) {
+        //userHash[socket.id] = 
+        io.emit("publish", socket.id, position, rotation);
+    });
+
+    socket.on("publish", function(position, rotation) {
+        io.emit("publish", socket.id, position, rotation);
+    });
+
+    socket.on("disconnect", function() {
+        io.emit("disconnect", socket.id);
+    });
 });
 
 http.listen(process.env.PORT || 8000, function() {
---
+++ public/js/area.js
@@ -0,0 +1,10 @@
+var Area = function() {
+    this.lowerLeftWalls = makeLowerLeftArea();
+};
+
+function makeLowerLeftArea() {
+    var walls = [];
+    walls.push(new Wall(new THREE.Vector3(-100, 0, 50), new THREE.Vector3(2, 100, 100)));
+    walls.push(new Wall(new THREE.Vector3(-50, 0, 100), new THREE.Vector3(100, 100, 2)));
+    return walls;
+}
