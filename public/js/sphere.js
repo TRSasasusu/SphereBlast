37636abface1794d4adc01e08e8d3a270945f897
kishi <kishiinnovation@gmail.com>
2017-01-03 16:11:28 +0900

    Change the way to move.

--- public/js/sphere.js
+++ public/js/sphere.js
@@ -28,7 +28,30 @@ var MySphere = function(modelResult) {
             objs[0].object.rotation.y += deltatime * 10;
         }*/
         var cameraDirection = forward(camera);
-        var sphereDirection = forward(this.modelResult.object);
+        var sphereForward = forward(this.modelResult.object);
+        var sphereRight = right(this.modelResult.object);
+        var sphereUp = up(this.modelResult.object);
+        if(cameraDirection.dot(sphereForward) > 0.9) {
+            this.velocity.add(sphereForward.multiplyScalar(deltatime * 0.0001));
+        }
+        else if(cameraDirection.dot(sphereForward) < -0.9) {
+            this.velocity.sub(sphereForward.multiplyScalar(deltatime * 0.0001));
+        }
+        else if(cameraDirection.dot(sphereRight) > 0.9) {
+            this.velocity.add(sphereRight.multiplyScalar(deltatime * 0.0001));
+        }
+        else if(cameraDirection.dot(sphereRight) < -0.9) {
+            this.velocity.sub(sphereRight.multiplyScalar(deltatime * 0.0001));
+        }
+        else if(cameraDirection.dot(sphereUp) > 0.9) {
+            this.velocity.add(sphereUp.multiplyScalar(deltatime * 0.0001));
+        }
+        else if(cameraDirection.dot(sphereUp) < -0.9) {
+            this.velocity.sub(sphereUp.multiplyScalar(deltatime * 0.0001));
+        }
+        this.velocity.clampLength(0, 0.01);
+
+        /*var sphereDirection = forward(this.modelResult.object);
         var dot = cameraDirection.dot(sphereDirection);
         if(dot > 0.9) {
             var diff = cameraDirection.sub(sphereDirection);
@@ -42,16 +65,16 @@ var MySphere = function(modelResult) {
             else {
                 this.velocity = new THREE.Vector3(0, 0, 0);
             }
-        }
+        }*/
         
         /*else if(dot > 0.9) {
             var diff = cameraDirection.sub(sphereDirection);
             this.modelResult.object.rotation.x += diff.y * deltatime;
             this.modelResult.object.rotation.y += diff.x * deltatime;
         }*/
-        else {
+        /*else {
             this.velocity = new THREE.Vector3(0, 0, 0);
-        }
+        }*/
 
         if(this.velocity.z < -500) {
             console.log(this.velocity);
@@ -63,6 +86,22 @@ function forward(object) {
     var matrix = new THREE.Matrix4();
     matrix.extractRotation(object.matrix);
     var direction = new THREE.Vector3(0, 0, 1);
+    //return matrix.multiplyVector3(direction).normalize().multiplyScalar(-1);
+    return direction.applyMatrix4(matrix).normalize().multiplyScalar(-1);
+}
+
+function right(object) {
+    var matrix = new THREE.Matrix4();
+    matrix.extractRotation(object.matrix);
+    var direction = new THREE.Vector3(1, 0, 0);
+    //return matrix.multiplyVector3(direction).normalize().multiplyScalar(-1);
+    return direction.applyMatrix4(matrix).normalize().multiplyScalar(-1);
+}
+
+function up(object) {
+    var matrix = new THREE.Matrix4();
+    matrix.extractRotation(object.matrix);
+    var direction = new THREE.Vector3(0, 1, 0);
     //return matrix.multiplyVector3(direction).normalize().multiplyScalar(-1);
     return direction.applyMatrix4(matrix).normalize().multiplyScalar(-1);
 }
