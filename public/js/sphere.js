12d85d36c68cb001b14f282ba253941967c84781
kishi <kishiinnovation@gmail.com>
2017-01-03 11:41:27 +0900

    Rotate object by raycast from camera.

--- public/js/sphere.js
+++ public/js/sphere.js
@@ -6,5 +6,18 @@ var MySphere = function(modelResult) {
             return;
         }
         this.modelResult.object.position += deltatime * this.velocity;
+
+        var ray = new THREE.Raycaster(camera.position, forward(camera).normalize().multiplyScalar(-1));
+        var objs = ray.intersectObjects(scene.children);
+        if(objs.length > 0) {
+            objs[0].object.rotation.y += deltatime * 10;
+        }
     };
 };
+
+function forward(object) {
+    var matrix = new THREE.Matrix4();
+    matrix.extractRotation(object.matrix);
+    var direction = new THREE.Vector3(0, 0, 1);
+    return matrix.multiplyVector3(direction);
+}
