6cad2a20ca3ae6f56427239bc7811aa2a5ddc709
kishi <kishiinnovation@gmail.com>
2017-01-06 02:50:07 +0900

    Make changing areas.

--- public/js/area.js
+++ public/js/area.js
@@ -2,8 +2,10 @@ var Area = function() {
     //this.lowerLeftWalls = makeLowerLeftArea();
     this.lowerLeftWalls = loadmodel('lowerleft', 'lowerleft', true);
     this.mountain = loadmodel('mountain', 'mountain', true);
+    this.castle = loadmodel('castle', 'castle', true);
     this.users = {};
     this.teleport = new Teleport(new THREE.Vector3(-2, 0, -8), new THREE.Vector3(-1000, 50, 0));
+    this.whichArea = WhichArea.MAZE;
     this.move = function() {
         if(this.lowerLeftWalls.isLoaded) {
             //this.lowerLeftWalls.object.position.set(-)
@@ -12,8 +14,34 @@ var Area = function() {
             this.mountain.object.position.x = -1000;
             this.mountain.object.scale.set(70, 70, 70);
         }
+        if(this.castle.isLoaded) {
+            this.castle.object.position.x = 1000;
+            this.castle.object.scale.set(70, 70, 70);
+        }
 
         this.teleport.move();
+        if(sphere.modelResult.isLoaded) {
+            if(sphere.modelResult.object.position.x < -500) {
+                if(this.whichArea != WhichArea.MOUNTAIN) {
+                    this.whichArea = WhichArea.MOUNTAIN;
+                    ChangeAudio('Nostalgia');
+                }
+            }
+            else if(sphere.modelResult.object.position.x > 500) {
+                this.whichArea = WhichArea.CASTLE;
+                if(this.whichArea != WhichArea.CASTLE) {
+                    this.whichArea = WhichArea.CASTLE;
+                    ChangeAudio('mataonajiyuugure');
+                }
+            }
+            else {
+                this.whichArea = WhichArea.MAZE;
+                if(this.whichArea != WhichArea.MAZE) {
+                    this.whichArea = WhichArea.MAZE;
+                    ChangeAudio('yorunotobari');
+                }
+            }
+        }
 
         for(var key in this.users) {
             if(this.users[key].previousTime < 0) {
@@ -25,6 +53,10 @@ var Area = function() {
             }
         }
     };
+};
+
+var WhichArea = {
+    MAZE: 0, MOUNTAIN: 1, CASTLE: 2
 };
 
 function makeLowerLeftArea() {
@@ -43,4 +75,9 @@ function makeLowerLeftArea() {
     walls.push(new Wall(new THREE.Vector3(-450, 0, 800), new THREE.Vector3(20, 1000, 200)));
     walls.push(new Wall(new THREE.Vector3(-100, 0, 900), new THREE.Vector3(200, 1000, 20)));
     return walls;
+}
+
+function ChangeAudio(audioName) {
+    audio.pause();
+    audio.currentTime = 0;
 }
