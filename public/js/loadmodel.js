0355aa4e3e39135d6316dbde47792731ee756584
kishi <kishiinnovation@gmail.com>
2017-01-07 11:04:24 +0900

    not require mtl files.

--- public/js/loadmodel.js
+++ public/js/loadmodel.js
@@ -12,7 +12,36 @@ function loadmodel(obj, mtl, isThereNormal) {
     var onError = function(xhr) {};
     var mtlLoader = new THREE.MTLLoader();
     var result = new ModelResult();
-    mtlLoader.load("models/" + mtl + ".mtl", function(materials) {
+
+    var texture = new THREE.TextureLoader().load('models/' + mtl + '_colored.png');
+    if(!(mtl in materialCaches)) {
+        if(mtl == 'sphere2') {
+            materialCaches[mtl] = new THREE.MeshBasicMaterial({color: 0xffffff, transparent: true, side: THREE.BackSide, map: texture});
+        }
+        else {
+            if(isThereNormal) {
+                var normalTexture = new THREE.TextureLoader().load('models/' + mtl + '_normal.png');
+                materialCaches[mtl] = new THREE.MeshPhongMaterial({color: 0xffffff, map: texture, normalMap: normalTexture});
+            }
+            else {
+                materialCaches[mtl] = new THREE.MeshPhongMaterial({color: 0xffffff, map: texture});
+            }
+        }
+    }
+    var objLoader = new THREE.OBJLoader();
+    objLoader.load("models/" + obj + ".obj", function(object) {
+        object.traverse(function(child) {
+            if(child instanceof THREE.Mesh) {
+                //child.material = basic;
+                child.material = materialCaches[mtl];
+            }
+        });
+        result.object = object;
+        object.position.set(0, 0, 0);
+        scene.add(object);
+        result.isLoaded = true;
+    }, onProgress, onError);
+    /*mtlLoader.load("models/" + mtl + ".mtl", function(materials) {
         materials.side = THREE.BackSide;
         materials.transparent = true;
         materials.opacity = 0.5;
@@ -47,7 +76,7 @@ function loadmodel(obj, mtl, isThereNormal) {
             scene.add(object);
             result.isLoaded = true;
         }, onProgress, onError);
-    });
+    });*/
 
     return result;
 }
